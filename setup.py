a8b56d8de05ac60d704969911960ea427103c45d
Heshy Roskes <heshyr@gmail.com>
2021-04-12 09:09:40 -0400

    metadata and gitignore

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 import setuptools
 
 setuptools.setup(
-  name="slurm-python-utils",
+  name = "slurm-python-utils",
   packages = setuptools.find_packages(include=["job_lock"]),
+  author = "Heshy Roskes",
+  author_email = "heshyr@gmail.com",
+  url = "https://github.com/hroskes/slurm-python-utils",
 )
