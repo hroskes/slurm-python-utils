29516a8a858ccf839a4170f5648ba24c00810fd8
Heshy Roskes <heshyr@gmail.com>
2021-04-13 08:17:57 -0400

    read readme

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
-import setuptools, sys
+import pathlib, setuptools, sys
 
 install_requires = []
 if sys.platform != "cygwin":
   install_requires.append("psutil")
+
+here = pathlib.Path(__file__).parent
+with open(here/'README.md', encoding='utf-8') as f:
+  long_description = f.read()
 
 setuptools.setup(
   name = "slurm-python-utils",
@@ -11,4 +15,6 @@ setuptools.setup(
   author_email = "heshyr@gmail.com",
   url = "https://github.com/hroskes/slurm-python-utils",
   install_requires = install_requires,
+  long_description = long_description,
+  long_description_content_type = 'text/markdown',
 )
