d958abd7ba08534aa6dc94124c7f66fbed792a52
Heshy Roskes <heshyr@gmail.com>
2021-06-24 08:23:37 -0400

    handle conditional dependency in a nicer way

--- setup.py
+++ setup.py
@@ -1,9 +1,5 @@
 import pathlib, setuptools, sys
 
-install_requires = []
-if sys.platform != "cygwin":
-  install_requires.append("psutil")
-
 here = pathlib.Path(__file__).parent
 with open(here/'README.md', encoding='utf-8') as f:
   long_description = f.read()
@@ -14,8 +10,9 @@ setuptools.setup(
   author = "Heshy Roskes",
   author_email = "heshyr@gmail.com",
   url = "https://github.com/hroskes/slurm-python-utils",
-  install_requires = install_requires,
+  install_requires = [
+    "psutil;sys_platform!='cygwin'",
+  ],
   long_description = long_description,
   long_description_content_type = 'text/markdown',
-  has_ext_modules=lambda: True,
 )
